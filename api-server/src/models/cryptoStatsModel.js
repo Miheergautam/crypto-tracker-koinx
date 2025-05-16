6dea3eb3aeaff3f71190792b3375fd186f7ffd06
Miheer Gautam <miheergautam04@gmail.com>
2025-05-16 19:18:14 +0530

    feat: add CrptoStats model

---
+++ api-server/src/models/cryptoStatsModel.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const CryptoStatSchema = new mongoose.Schema({
+  coinId: { type: String },
+  price: { type: Number },
+  usd_market_cap: { type: Number },
+  usd_24h_vol: { type: Number },
+  usd_24h_change: { type: Number },
+  timestamp: { type: Date, default: Date.now },
+});
+
+const CryptoStats = mongoose.model("CryptoStat", CryptoStatSchema);
+
+export default CryptoStats;
