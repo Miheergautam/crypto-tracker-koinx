ed3e4122edbb74f5d957d973a77d07cffd0639c3
Miheer Gautam <miheergautam04@gmail.com>
2025-05-16 22:33:51 +0530

    feat: add kafka consumer in api-server

---
+++ api-server/src/kafkaConsumer.js
@@ -0,0 +1,34 @@
+import { Kafka } from "kafkajs";
+import { storeCryptoStats } from "./services/cryptoService.js";
+
+const kafka = new Kafka({
+  clientId: "api-server",
+  brokers: ["localhost:9092"],
+});
+
+const consumer = kafka.consumer({ groupId: "crypto-group" });
+
+export const startConsumer = async () => {
+  await consumer.connect();
+  console.log("Kafka Consumer connected");
+
+  await consumer.subscribe({ topic: "crypto-update", fromBeginning: false });
+
+  await consumer.run({
+    eachMessage: async ({ message }) => {
+      const msgValue = message.value.toString();
+      console.log("Received message:", msgValue);
+
+      try {
+        const data = JSON.parse(msgValue);
+        if (data.trigger === "update") {
+          console.log("Triggering storeCryptoStats()...");
+          await storeCryptoStats();
+          console.log("Crypto stats updated");
+        }
+      } catch (err) {
+        console.error("Error processing Kafka message", err);
+      }
+    },
+  });
+};
